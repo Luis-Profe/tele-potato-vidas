229f8245c59468fce920ef8014cffc1d99884cf9
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-05-24 13:30:17 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,19 +9,25 @@ input.onGesture(Gesture.Shake, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     potato = randint(10, 20)
+    while (vidas > 0) {
+        if (potato == 0) {
+            basic.showIcon(IconNames.Skull)
+            vidas += -1
+        }
+        if (potato < 0) {
+            basic.clearScreen()
+        }
+        if (potato > 0) {
+            basic.showIcon(IconNames.Target)
+            potato += -1
+        }
+    }
 })
+let vidas = 0
 let potato = 0
 radio.setGroup(1)
 potato = -1
+vidas = 3
 basic.forever(function () {
-    if (potato == 0) {
-        basic.showIcon(IconNames.Skull)
-    }
-    if (potato < 0) {
-        basic.clearScreen()
-    }
-    if (potato > 0) {
-        basic.showIcon(IconNames.Target)
-        potato += -1
-    }
+	
 })
